454798111a7b55113a6d7e15b2c21cec8bbdf29c
LeeHyeongKon <ksy37667@gmail.com>
2020-02-09 15:32:50 +0900

    login
    login form / login logic

--- shopuser/forms.py
+++ shopuser/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.hashers import check_password, make_password
 from .models import Shopuser
 
 class RegisterForm(forms.Form):
@@ -36,6 +37,39 @@ class RegisterForm(forms.Form):
             else:
                 shopuser = Shopuser(
                     email = email,
-                    password = password
+                    password = make_password(password)
                 )
                 shopuser.save()
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required' : '이메일을 입력해주세요'
+        },
+        max_length=64, label='이메일'
+    )
+
+    password = forms.CharField(
+        error_messages={
+            'required' : '비밀번호를 입력해주세요.'
+        },
+        widget=forms.PasswordInput, label='비밀번호'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        if email and password:
+            try:
+                shopuser = Shopuser.objects.get(email=email)
+            except Shopuser.DoesnotExist:
+                self.add_error('email', '아이디가 없습니다.')
+                return
+            
+            if not check_password(password, shopuser.password):
+                self.add_error('password', '비밀번호를 틀렸습니다.')
+            else:
+                self.user_id = shopuser.id
