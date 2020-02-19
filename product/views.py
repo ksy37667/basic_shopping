26d49e31ed5b3b5bcea37991b26e51ff6a75ae87
LeeHyeongKon <ksy37667@gmail.com>
2020-02-19 10:51:38 +0900

    order
    order

--- product/views.py
+++ product/views.py
@@ -27,6 +27,5 @@ class ProductDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         context['form'] = OrderForm()
         return context
