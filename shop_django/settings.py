38f8e86337626805b0ca2099f24d28a21039e4f6
LeeHyeongKon <ksy37667@gmail.com>
2020-02-24 11:24:08 +0900

    a

--- shop_django/settings.py
+++ shop_django/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
